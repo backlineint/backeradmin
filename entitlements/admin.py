48b74755b1ce6ec0c004d4a7f58b35513fa52fb3
backlineint <contact.brianperry@gmail.com>
2013-02-26 22:57:32 +0000

    Fixed entitlement model and exposed in admin

--- entitlements/admin.py
+++ entitlements/admin.py
@@ -3,6 +3,8 @@ from entitlements.models import Issue, Entitlement
 
 class IssueAdmin(admin.ModelAdmin):
     list_display = ('description', 'issue_id', 'external_issue_id', 'publish_time')
+class EntitlementAdmin(admin.ModelAdmin):
+    list_display = ('user', 'issue')
 
 admin.site.register(Issue, IssueAdmin)
-admin.site.register(Entitlement)
+admin.site.register(Entitlement, EntitlementAdmin)
--- entitlements/models.py
+++ entitlements/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-
 class Issue(models.Model):
     issue_id = models.IntegerField()
     external_issue_id = models.IntegerField()
     publish_time = models.DateTimeField('date published')
     description = models.CharField(max_length=200)
+    def __unicode__(self):
+        return self.description
+
+class Entitlement(models.Model):
+    user = models.ForeignKey(User)
+    issue = models.ForeignKey(Issue)
