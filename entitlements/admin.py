082aeae4b131cef478fe6cb3a801378662d58a26
backlineint <contact.brianperry@gmail.com>
2013-02-26 20:33:49 +0000

    removed entitlements model

--- entitlements/admin.py
+++ entitlements/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from entitlements.models import Issue
+from entitlements.models import Issue, Entitlement
 
 class IssueAdmin(admin.ModelAdmin):
     list_display = ('description', 'issue_id', 'external_issue_id', 'publish_time')
 
 admin.site.register(Issue, IssueAdmin)
+admin.site.register(Entitlement)
