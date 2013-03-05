65a6bc470fa03aa42aeb3bf67cc27dad4a26f80b
backlineint <contact.brianperry@gmail.com>
2013-03-05 20:58:47 +0000

    Get all entitlements has working json response and dynamically pulls session token from URL

--- entitlements/models.py
+++ entitlements/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+#from django.core.serializers.python import Serializer
 
 class Issue(models.Model):
     issue_id = models.IntegerField()
--- entitlements/views.py
+++ entitlements/views.py
@@ -1,10 +1,30 @@
 # Create your views here.
 from django.http import HttpResponse
-from entitlements.models import Entitlement
+from entitlements.models import Entitlement, Issue
+from django.utils import simplejson
+from django.contrib.sessions.models import Session
+from django.contrib.auth.models import User
 
 def get_entitlements(request):
-	# currently we're just returning a particular user ID
-	# TODO: filter dynamically based on session ID
-	output = Entitlement.objects.filter(user=2)
-	# TODO: Change this to be expected JSON response - could start with displaying fields if necessary
-	return HttpResponse(output)
+	# TODO: Test for shared secret
+	# Parse the session token from the URL
+	session_key = request.GET.get('session_token')
+	# Grab the session object for this session token and derive the username
+	session = Session.objects.get(session_key=session_key)
+        uid = session.get_decoded().get('_auth_user_id')
+        user = User.objects.get(pk=uid)
+        username =  user.username
+	# Work in progress below here
+	results = Entitlement.objects.get(user=2)
+	# TODO - Handle getting multiple entitlements
+	#results = Entitlement.objects.filter(user=2)
+	#username = results.user
+	# TODO - Change this to get the right ID
+	issue = results.issue
+	#, user.get_full_name(), user.email
+    	to_json = {
+        	"username": str(username),
+        	"issue": str(issue),
+		"session_token": str(session_key)
+    	}
+	return HttpResponse(simplejson.dumps(to_json), content_type='application/json')
