092f99da7dac3f58395a40a20602ebb53e5c0d4c
Brian Perry <contact.brianperry@gmail.com>
2013-03-20 20:50:29 +0000

    Changed entitlements form in admin to use search for users

--- entitlements/admin.py
+++ entitlements/admin.py
@@ -4,6 +4,7 @@ from entitlements.models import Issue, Entitlement
 class IssueAdmin(admin.ModelAdmin):
     list_display = ('description', 'issue_id', 'external_issue_id', 'publish_time')
 class EntitlementAdmin(admin.ModelAdmin):
+    raw_id_fields = ('user',)
     list_display = ('user', 'issue')
 
 admin.site.register(Issue, IssueAdmin)
